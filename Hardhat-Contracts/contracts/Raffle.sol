5d30b0468d1105a83d2cf754d9df33410099d603
Ishrani Rajiv <rajivishrani@outlook.com>
2022-09-29 19:57:21 +0530

    fixed: typo in contract

--- Hardhat-Contracts/contracts/Raffle.sol
+++ Hardhat-Contracts/contracts/Raffle.sol
@@ -14,7 +14,7 @@ error Raffle__SendMoreToEnterRaffle();
 error Raffle__RaffleNotOpen();
 
 /**@title A sample Raffle Contract
- * @author Patrick Collins
+ * @author Rajiv Ishrani
  * @notice This contract is for creating a sample raffle contract
  * @dev This implements the Chainlink VRF Version 2
  */
